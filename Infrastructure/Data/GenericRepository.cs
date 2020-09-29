79720b89a435014b2d9d20d0395766eb064c296d
FaraKhalili <FaraKhalili@github.com>
2020-09-28 19:38:03 -0600

    add sort and search filter

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -33,15 +33,19 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
         {
-            return await  ApplySpecification(spec).ToListAsync();
+            return await ApplySpecification(spec).ToListAsync();
         }
 
-        private IQueryable<T> ApplySpecification (ISpecification<T> spec)
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+        
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
 
         {
-            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(),spec);
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
-
 
 
     }
